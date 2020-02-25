90b4c08f15698d3d9fefbc6f8525969f34bfd41a
asijaw <asija.w@yahoo.com>
2020-02-25 09:07:41 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,102 @@
+INSERT INTO users(name, age) VALUES('A', 1);
+INSERT INTO users(name, age) VALUES('B', 2);
+INSERT INTO users(name, age) VALUES('C', 3);
+INSERT INTO users(name, age) VALUES('D', 4);
+INSERT INTO users(name, age) VALUES('E', 5);
+INSERT INTO users(name, age) VALUES('F', 6);
+INSERT INTO users(name, age) VALUES('G', 7);
+INSERT INTO users(name, age) VALUES('H', 8);
+INSERT INTO users(name, age) VALUES('I', 9);
+INSERT INTO users(name, age) VALUES('J', 10);
+INSERT INTO users(name, age) VALUES('K', 11);
+INSERT INTO users(name, age) VALUES('L', 12);
+INSERT INTO users(name, age) VALUES('M', 13);
+INSERT INTO users(name, age) VALUES('N', 14);
+INSERT INTO users(name, age) VALUES('O', 15);
+INSERT INTO users(name, age) VALUES('P', 16);
+INSERT INTO users(name, age) VALUES('Q', 17);
+INSERT INTO users(name, age) VALUES('R', 18);
+INSERT INTO users(name, age) VALUES('S', 19);
+INSERT INTO users(name, age) VALUES('T', 20);
+
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) 
+    VALUES('Painting', 'Art', 1500, '1/2/20', '12/3/20');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) 
+    VALUES('Drawing', 'Art', 1000, '1/2/20', '12/3/20');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) 
+    VALUES('Photography', 'Art', 700, '1/2/20', '12/3/20');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) 
+    VALUES('Chemistry', 'Science', 5000, '2/10/20', '5/3/20');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) 
+    VALUES('Biology', 'Science', 5000, '2/10/20', '5/3/20');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) 
+    VALUES('Psychology', 'Science', 5000, '2/10/20', '5/3/20');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) 
+    VALUES('Technology', 'Science', 10000, '2/10/20', '5/3/20');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) 
+    VALUES('Physics', 'Science', 3000, '2/10/20', '5/3/20');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) 
+    VALUES('Calculus', 'Math', 500, '5/15/20', '10/31/20');
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) 
+    VALUES('Algebra', 'Math', 500, '5/15/20', '10/31/20');
+
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(100, 1, 1);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(200, 2, 1);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(250, 3, 1);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(100, 4, 1);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(60, 5, 1);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(50, 6, 1);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(100, 7, 1);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(70, 8, 1);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(150, 9, 1);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(100, 10, 1);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(500, 11, 2);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(180, 12, 2);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(240, 13, 2);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(300, 14, 2);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(100, 15, 2);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(700, 16, 2);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(300, 17, 2);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(400, 18, 2);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(180, 19, 2);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(240, 20, 2);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(100, 1, 3);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(50, 2, 3);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(70, 3, 3);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(90, 4, 3);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(50, 5, 3);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(40, 6, 3);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(120, 7, 3);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(150, 8, 3);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(40, 9, 3);
+INSERT INTO pledges(amount, user_id, project_id) 
+    VALUES(90, 10, 3);
